b2da46a9d479f5d6c54d2914297fbea86ba71909
Ahmed Hany Farouk <67925988+Ahmed-H300@users.noreply.github.com>
2023-12-27 00:19:51 +0200

    [DONE] Lab 5

--- Lab 5 - Named Entity Recognition/utils.py
+++ Lab 5 - Named Entity Recognition/utils.py
@@ -1,6 +1,6 @@
 def get_vocab(vocab_path, tags_path):
     vocab = {}
-    with open(vocab_path) as f:
+    with open(vocab_path, 'r', encoding='utf-8') as f:
         for i, l in enumerate(f.read().splitlines()):
             vocab[l] = i  # to avoid the 0
         # loading tags (we require this to map tags to their indices)
@@ -16,7 +16,7 @@ def get_params(vocab, tag_map, sentences_file, labels_file):
     sentences = []
     labels = []
 
-    with open(sentences_file) as f:
+    with open(sentences_file, 'r', encoding='utf-8') as f:
         for sentence in f.read().splitlines():
             # replace each token by its index if it is in vocab
             # else use index of UNK_WORD
